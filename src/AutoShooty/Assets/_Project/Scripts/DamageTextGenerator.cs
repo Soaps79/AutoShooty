278bf99a540a3cee92d0074f0380326559d605de
Soaps79 <dansopel@gmail.com>
2023-09-04 18:49:00 -0400

    Basic damage text

---
+++ src/AutoShooty/Assets/_Project/Scripts/DamageTextGenerator.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using QGame;
+using System;
+using TMPro;
+using DG.Tweening;
+
+[RequireComponent(typeof(Combatant))]
+public class DamageTextGenerator : QScript
+{
+    private Combatant _combatant;
+
+    [SerializeField]
+    private TMP_Text _normalPrefab;
+    [SerializeField]
+    private TMP_Text _criticalPrefab;
+    [SerializeField]
+    private float _moveTime;
+    [SerializeField]
+    private float _moveDistance;
+
+    private void Awake()
+    {
+        _combatant = gameObject.GetComponent<Combatant>();
+        _combatant.OnDamageTaken += HandleDamageTaken;
+    }
+
+    private void HandleDamageTaken(float damage, bool isCritical)
+    {
+        var text = Instantiate(isCritical ? _criticalPrefab : _normalPrefab);
+        text.transform.position = transform.position;
+        text.text = damage.ToString("N0");
+        text.transform.DOMoveY(transform.position.y + _moveDistance, _moveTime)
+            .onComplete += () => { GameObject.Destroy(text); };
+    }
+}
